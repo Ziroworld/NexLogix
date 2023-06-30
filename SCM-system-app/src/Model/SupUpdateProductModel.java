2178506e3eff47b82df15a7152acbcd8989643df
Rohan manandhaar <118821550+Ziroworld@users.noreply.github.com>
2023-06-30 11:21:47 +0545

    Modified the Fetched data from other contributors and added the model code for the supplier dashboard features
    Modified the Fetched data from other contributors and added the model code for the supplier dashboard features like update  product and update self

---
+++ SCM-system-app/src/Model/SupUpdateProductModel.java
@@ -0,0 +1,42 @@
+
+package Model;
+
+public class SupUpdateProductModel {
+    private int pid;
+    private String productname; 
+    private Double productprice;
+    
+    public  SupUpdateProductModel( int pid, String productname, Double productprice){
+        this.pid = pid;
+        this.productname = productname;
+        this.productprice = productprice;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public Double getProductprice() {
+        return productprice;
+    }
+
+    public void setProductprice(Double productprice) {
+        this.productprice = productprice;
+    }
+
+
+    
+    
+}
