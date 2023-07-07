511808fbba44097c352c73f125ae9f14962b5447
bibekkoirala1 <126053729+bibekkoirala1@users.noreply.github.com>
2023-07-07 10:18:17 +0545

    added backend code of supplier username
    added backend code of database, model and controller

---
+++ SCM-system-app/src/Model/SupUpdateUsernameModel.java
@@ -0,0 +1,42 @@
+
+package Model;
+
+public class SupUpdateUsernameModel {
+    private String oldusername;
+    private String newusername;
+    private String password;
+    
+    public SupUpdateUsernameModel(String oldusername, String newusername, String password){
+        this.oldusername = oldusername;
+        this.newusername = newusername;
+        this.password = password;
+    }
+
+    public String getOldusername() {
+        return oldusername;
+    }
+
+    public void setOldusername(String oldusername) {
+        this.oldusername = oldusername;
+    }
+
+    public String getNewusername() {
+        return newusername;
+    }
+
+    public void setNewusername(String newusername) {
+        this.newusername = newusername;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public boolean hasEmptyFields() {
+        return oldusername.isEmpty() || newusername.isEmpty() || password.isEmpty();
+    }
+}
